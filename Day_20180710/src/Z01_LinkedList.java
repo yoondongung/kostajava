281550e4e17234769337ae6a1c12c718647b4895
yoondongung <qwe7662@naver.com>
2018-07-10 11:06:13 +0900

    .

--- Day_20180710/src/Z01_LinkedList.java
+++ Day_20180710/src/Z01_LinkedList.java
@@ -1,4 +1,6 @@
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * List 계열 : 순서 있음, 중복허용됨
@@ -20,6 +22,47 @@ public class Z01_LinkedList {
 		
 		ll.push("강아지");	// 큐를 구현
 		System.out.println(ll);
+		
+		ll.add(2, "야옹이");	// 비순차적 삽입
+		System.out.println(ll);
+		
+		ll.remove(2);	// 비순차적 삭제
+		System.out.println(ll);
+		
+		System.out.println("향상된 for문");
+		for(String a : ll) {
+			System.out.print(a);
+		}
+		
+		System.out.println();
+		System.out.println("일반 for문");
+		for (int i = 0; i < ll.size(); i++) {
+			System.out.print(ll.get(i));
+		}
+		
+		//iterator	: 단방향 순회를 할 수 있는 인터페이스
+		Iterator<String> iter = ll.iterator();	// 일회용 발생기
+		while(iter.hasNext()) {	// 다음 원소가 있느냐?
+			String s = iter.next();
+			System.out.println(s+",");
+		}
+		System.out.println(": 이터레이터 사용");
+		
+		
+		//Iterator : 양방향 순회를 할 수 있는 인터페이스
+		ListIterator<String> listiter = ll.listIterator();
+		while(listiter.hasNext()) {
+			String s = listiter.next();
+			System.out.print(s);
+		}
+		System.out.println(": 리스트 이터레이터 사용");
+		
+		
+		while(listiter.hasPrevious()) {
+			String s = listiter.previous();
+			System.out.print(s);
+		}
+		System.out.println(": 리스트 이터레이터 사용");
 		
 	}	// end of main
 }	// end of class
